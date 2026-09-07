fd2882c2f4974365aaf1ccf2470accfd2b33dd35
Sebastian Thunberg <sebastian.thunberg@appliedtechnology.se>
2026-09-07 10:42:27 +0200

    feat: Add enums for Views and DBType to define application states and database types

---
+++ src/webview/types/enums.ts
@@ -0,0 +1,14 @@
+export enum Views {
+  CONNECTION = "CONNECTION",
+  SCHEMA = "SCHEMA",
+  QUERY = "QUERY",
+  OUTPUT = "OUTPUT",
+}
+
+export enum DBType {
+  POSTGRES = "postgres",
+  MYSQL = "mysql",
+  SQLITE = "sqlite",
+  MSSQL = "mssql",
+  ORACLE = "oracle",
+}
